efacea40e7e825babc6580c2826038190d89bb7e
Abhilash3 <abhi211193@gmail.com>
2015-01-23 03:09:01 -0500

    Create Parenthesis.java

---
+++ src/vo/Parenthesis.java
@@ -0,0 +1,32 @@
+package vo;
+
+import interfaces.IVisitable;
+import interfaces.IVisitor;
+
+public class Parenthesis implements IVisitable {
+	
+	private IVisitable node;
+	
+	public Parenthesis(IVisitable node) {
+		this.node = node;
+	}
+
+	public IVisitable getNode() {
+		return node;
+	}
+
+	public void setNode(IVisitable node) {
+		this.node = node;
+	}
+	
+	@Override
+	public String toString() {
+		return this.node.toString();
+	}
+
+	@Override
+	public void accept(IVisitor visitor) {
+		visitor.visit(this);
+	}
+
+}
